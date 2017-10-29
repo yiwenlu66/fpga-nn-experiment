eb42472c4ba98e791a03938cf0fdce253d70f752
Peter Lu <mcfloundinho@gmail.com>
2017-10-29 16:37:33 +0800

    matlab: fix an error in cost function

--- matlab/logistic/costFunction.m
+++ matlab/logistic/costFunction.m
@@ -2,7 +2,7 @@ function [J, grad] = costFunction(X, y, Theta)
     % compute cost for logistic regression
     m = size(X, 1);
     h = sigmoid(X * Theta);
-    J = (1 / m) * (-y' * log(h) + (1 - y') * log(1 - h));
+    J = (1 / m) * (-y' * log(h) - (1 - y') * log(1 - h));
     J = sum(sum(J));
     grad = (1 / m) * X' * (h - y);
 end
